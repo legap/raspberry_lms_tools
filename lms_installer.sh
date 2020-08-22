0752ae35f77fa90d93e2697421520440162b977e
Pascal Gafner <gafner.pascal@gmail.com>
2020-08-22 22:11:54 +0200

    Update lms_installer.sh
    cleanup in installer script

--- lms_installer.sh
+++ lms_installer.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
+# enter your prefered version here - default is the latest nightly build
 base_url=http://downloads.slimdevices.com/nightly/index.php?ver=7.9
+
 # get latest nightly build url
 echo "getting latest version from url: ${base_url}"
 latest_nightly=$(curl -s ${base_url} | grep arm.deb | sed -r 's/.*href="\.(.*)".*/\1/')
-
-# update apt cache
-# sudo apt update
 
 # download current .deb file into the apt cache
 download_url="http://downloads.slimdevices.com/nightly${latest_nightly}"
 echo "using download url: ${download_url}"
 sudo wget ${download_url}
-
-# run an apt full-upgrade to install the latest package
-# sudo apt full-upgrade
 
 # get the package name
 package_name=$(echo ${latest_nightly} | sed -r 's/.*\/(.*)/\1/')
